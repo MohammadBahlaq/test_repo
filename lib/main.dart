088413ab5bf06603de20e059df19235f6d5352df
Mohammad Bahlaq <Bahlaq57@gmail.com>
2025-09-29 12:46:58 +0300

    test easy

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
+      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Home Page'),
+      home: const MyHomePage(title: 'Flutter Home Demo Page'),
     );
   }
 }
